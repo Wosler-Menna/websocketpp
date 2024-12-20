d4d0a884398d7a9897b582c56e045a50f0d14c39
Wosler-Menna <menna.rihan@wosler.ca>
2024-12-20 16:03:01 -0500

    changed io_service to io_context for boost 1.87

--- examples/external_io_service/external_io_service.cpp
+++ examples/external_io_service/external_io_service.cpp
@@ -59,7 +59,7 @@ void on_message(ws_echo_server* s, websocketpp::connection_hdl hdl, ws_echo_serv
 }
 
 int main() {
-    asio::io_service service;
+    asio::io_context service;
 
     // Add a TCP echo server on port 9003
     tcp_echo_server custom_http_server(service, 9003);
@@ -70,7 +70,7 @@ int main() {
     ws_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
 
     // The only difference in this code between an internal and external
-    // io_service is the different constructor to init_asio
+    // io_context is the different constructor to init_asio
     ws_server.init_asio(&service);
 
     // Register our message handler
@@ -80,6 +80,6 @@ int main() {
 
     // TODO: add a timer?
 
-    // Start the Asio io_service run loop for all
+    // Start the Asio io_context run loop for all
     service.run();
 }
